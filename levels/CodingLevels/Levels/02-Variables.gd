8ea581b34e6a5341b5f3485ea8197abdf960298e
Timothy Karani <kartimothy@gmail.com>
2020-12-07 13:48:11 +0300

    Themes are now modular

--- levels/CodingLevels/Levels/02-Variables.gd
+++ levels/CodingLevels/Levels/02-Variables.gd
@@ -1,6 +1,8 @@
 extends "res://levels/CodingLevels/Levels/HttpReqShared.gd"
 
 export (GDScript) var coding_resources_object
+export (Theme) var interactive_session_theme
+export (Theme) var output_panel_theme
 
 onready var coding_ground = $CanvasLayer/InteractiveSession/Full/Right/Top/CodingGround
 onready var debug_output = $CanvasLayer/InteractiveSession/Full/Right/Bottom/TabContainer/Build/OutputMargin/Output
@@ -16,6 +18,9 @@ func _ready():
 		print_debug("Error while connecting to req_completed signal")
 	coding_resources = coding_resources_object.new()
 	coding_ground.text = coding_resources.code
+	$CanvasLayer/InteractiveSession.set_theme(interactive_session_theme)
+	debug_output.set_theme(output_panel_theme)
+	$CanvasLayer/InteractiveSession/Alert.set_theme(output_panel_theme)
 
 
 func _on_InteractiveSession_build():
